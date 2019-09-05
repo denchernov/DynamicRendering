ac7b509b899e10117fc804fa83dc7695997a22ee
Vasily Lukanin <v.lukanin@vlkextern.nl>
2019-09-05 18:59:03 +0200

    Header navigation - completed

--- Feature/Navigation/code/Controllers/NavigationController.cs
+++ Feature/Navigation/code/Controllers/NavigationController.cs
@@ -1,15 +1,85 @@
 ﻿namespace HelixHub.Feature.Navigation.Controllers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Web.Mvc;
-    using System.Web.UI.WebControls;
 
+    using HelixHub.Feature.Navigation.Extensions;
+    using HelixHub.Feature.Navigation.Models;
+
+    using Sitecore;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
     using Sitecore.Mvc.Controllers;
 
     public class NavigationController : SitecoreController
     {
+        private readonly ID contentPageTemplateId = new ID("{53B3F315-7E49-4512-8122-A096E3597DAA}");
+
         public ActionResult Header()
         {
-            return View("~/Views/Navigation/Header.cshtml");
+            var model = this.GetHeaderMenu();
+
+            return this.View("~/Views/Navigation/Header.cshtml", model);
+        }
+
+        private Header GetHeaderMenu()
+        {
+            if (Context.Site == null || string.IsNullOrEmpty(Context.Site.StartPath))
+            {
+                return null;
+            }
+
+            var homeItem = Context.Site.Database.GetItem(Context.Site.StartPath);
+            if (homeItem == null)
+            {
+                return null;
+            }
+
+            var header = new Header
+                             {
+                                 HomeItem = new NavigationItem { Title = homeItem.DisplayName, Link = homeItem.Url() },
+                                 MenuItems = new List<NavigationItem>()
+                             };
+
+            var childList = homeItem.GetChildren();
+            foreach (var child in childList)
+            {
+                var item = child as Item;
+                if (item == null)
+                {
+                    continue;
+                }
+
+                // If item is not content page, then skip
+                if (item.TemplateID != this.contentPageTemplateId)
+                {
+                    continue;
+                }
+
+                header.MenuItems.Add(
+                    new NavigationItem { IsActive = IsActive(item), Title = item.DisplayName, Link = item.Url() });
+            }
+
+            return header;
+        }
+
+        private static bool IsActive(Item navigationLinkItem)
+        {
+            if (navigationLinkItem == null)
+            {
+                return false;
+            }
+
+            var currentItem = Context.Item;
+            if (currentItem == null)
+            {
+                return false;
+            }
+
+            return currentItem.Paths.ContentPath.StartsWith(
+                navigationLinkItem.Paths.ContentPath,
+                StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
---
+++ Feature/Navigation/code/Models/Header.cs
@@ -0,0 +1,11 @@
+﻿namespace HelixHub.Feature.Navigation.Models
+{
+    using System.Collections.Generic;
+
+    public class Header
+    {
+        public NavigationItem HomeItem { get; set; }
+
+        public List<NavigationItem> MenuItems { get; set; }
+    }
+}
