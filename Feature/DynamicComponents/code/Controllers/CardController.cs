7e926c8031761bcc14456cabded40583047e1168
Denis Chernov <dchernov@arke.com>
2019-09-05 17:22:30 +0200

    Content items for layout components

---
+++ Feature/DynamicComponents/code/Controllers/CardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Sitecore.Mvc.Controllers;
+
+namespace HelixHub.Feature.DynamicComponents.Controllers
+{
+    public class CardController : SitecoreController
+    {
+        public ActionResult Card()
+        {
+            return View("~/Views/DynamicComponents/Card/Card.cshtml");
+        }
+    }
+}
---
+++ Feature/DynamicComponents/code/Controllers/LayoutController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Sitecore.Mvc.Controllers;
+
+namespace HelixHub.Feature.DynamicComponents.Controllers
+{
+    public class LayoutController : SitecoreController
+    {
+        public ActionResult Container()
+        {
+            return View("~/Views/DynamicComponents/Layout/Container.cshtml");
+        }
+
+        public ActionResult Row()
+        {
+            return View("~/Views/DynamicComponents/Layout/Row.cshtml");
+        }
+    }
+}
