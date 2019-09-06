57fdd77285d523e9c48a7f3d9bd5a7401089cf94
Denis Chernov <dchernov@arke.com>
2019-09-06 08:13:33 +0200

    Column, Ro, and Content components were added.

--- Feature/DynamicComponents/code/Controllers/LayoutController.cs
+++ Feature/DynamicComponents/code/Controllers/LayoutController.cs
@@ -1,18 +1,32 @@
 ﻿using System.Web.Mvc;
+using HelixHub.Feature.DynamicComponents.Models;
+using Sitecore;
 using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+using Convert = System.Convert;
 
 namespace HelixHub.Feature.DynamicComponents.Controllers
 {
     public class LayoutController : SitecoreController
     {
-        public ActionResult Container()
+        public ActionResult Column()
         {
-            return View("~/Views/DynamicComponents/Layout/Container.cshtml");
+            var renderingParameters = RenderingContext.Current.Rendering.Parameters;
+            var colNumberString = renderingParameters["ColNumber"];
+            var model = new ColumnModel {ColumnNumber = Convert.ToInt32(colNumberString)};
+
+
+            return View("~/Views/DynamicComponents/Layout/Column.cshtml", model);
         }
 
         public ActionResult Row()
         {
             return View("~/Views/DynamicComponents/Layout/Row.cshtml");
+        }
+
+        public ActionResult Content()
+        {
+            return View("~/Views/DynamicComponents/Layout/Content.cshtml");
         }
     }
 }
